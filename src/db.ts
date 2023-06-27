6c2cc0d3a5e12a103bf985bdc52545c2bfeab62e
Daniel Ramirez <63734600+danielru15@users.noreply.github.com>
2023-06-27 02:22:02 -0500

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -6,7 +6,7 @@ export const AppDataSource = new DataSource({
     host: "localhost",
     port: 5432,
     username: "postgres",
-    password: "9715",
+    password: "",
     database: "typeorm",
     entities: [users],
     synchronize:true,
